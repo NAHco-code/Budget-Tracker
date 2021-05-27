92a96ff358d9237f3317e347331fa9be500d5a52
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-05-26 23:27:28 -0500

    Docs: organize server file

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 const express = require("express");
 const logger = require("morgan");
+// const compression = require("compression");
 const mongoose = require("mongoose");
-const compression = require("compression");
 
-const PORT = 3000;
-
+const PORT = 3001;
 const app = express();
 
-app.use(logger("dev"));
-
-app.use(compression());
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(compression());
+app.use(logger("dev"));
 
-app.use(express.static("public"));
+// routes
+app.use(require("./routes/api.js"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
-// routes
-app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
