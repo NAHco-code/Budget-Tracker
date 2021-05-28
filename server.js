bb673f0f98d4ec7dedff4597b9882773b778cb34
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-05-28 13:49:46 -0500

    Docs: remove compression from active code to allow server to run

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(compression());
+// app.use(compression());
 app.use(logger("dev"));
 
 // routes
