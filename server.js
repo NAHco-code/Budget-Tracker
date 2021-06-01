acfbb45f2ea772a7c0c0e325b10626235b1b465f
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-05-31 19:28:59 -0500

    Debug: reinstall dependecies

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const compression = require("compression");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger("dev"));
